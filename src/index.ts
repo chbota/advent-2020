5dcbe87bfa34ca7c39bbf5bf11c79e51c4526b16
Chris Botaish <chbota@users.noreply.github.com>
2020-12-02 10:28:58 -0500

    Solve dec 2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import * as DecOne from './Dec1';
+import * as DecTwo from './Dec2';
 
-console.log(DecOne.findSumTo2020(DecOne.loadData()))
+console.group('December 1');
+console.log(DecOne.findSumTo2020(DecOne.loadData()));
 console.log(DecOne.findSum(DecOne.loadData(), 2020, 3));
+console.groupEnd();
+
+
+console.group('December 2');
+console.log(DecTwo.getNumValidPasswords(DecTwo.loadPasswordsAndPolicies(), DecTwo.letterCountPasswordValidator));
+console.log(DecTwo.getNumValidPasswords(DecTwo.loadPasswordsAndPolicies(), DecTwo.oneLetterAtPositionsPasswordValidator));
+console.groupEnd();
