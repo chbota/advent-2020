2d12b8d2fb4f2c2ab16fd7dd39fe4642c17229f9
Chris Botaish <chbota@users.noreply.github.com>
2020-12-10 09:23:52 -0500

    Delete unused import

--- src/Dec10/index.ts
+++ src/Dec10/index.ts
@@ -1,6 +1,5 @@
 import * as fs from "fs";
 import * as path from "path";
-import { addEdge, getOrCreateNode, Graph } from "../Graph";
 
 function checkAdapterCompatibility(
   outletJoltage: number,
